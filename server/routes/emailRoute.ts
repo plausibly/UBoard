2ba79afda03e51bb3284a677e3c14c763c3f3f92
plausibly <pouyaghi123@gmail.com>
2021-10-15 17:22:29 -0400

    f

--- server/routes/emailRoute.ts
+++ server/routes/emailRoute.ts
@@ -1,36 +1,32 @@
 import { Router, Request, Response } from "express";
-import { confirmEmail, checkEmailToken, resetPassword } from "../controllers/mailService";
- 
+import {
+  confirmEmail,
+  checkEmailToken,
+  resetPassword,
+} from "../controllers/mailService";
+
 const emailRoute = Router();
 
 /* Account Email Verification */
 emailRoute.get("/v=:token", async (req: Request, res: Response) => {
-    const status = await confirmEmail(req.params.token);
-    if (status)
-        res.send("Email succesfully confirmed");
-    else // Invalid token or error occurred
-        res.sendStatus(404);
-
+  const status = await confirmEmail(req.params.token);
+  if (status) res.send("Email succesfully confirmed");
+  // Invalid token or error occurred
+  else res.sendStatus(404);
 });
-
 
 /* Password Resets */
 emailRoute.get("/r=:token/p=:newpw", (req: Request, res: Response) => {
+  const decodedToken = checkEmailToken(req.params.token, "pw");
 
-    const decodedToken = checkEmailToken(req.params.token, "pw");
+  if (!decodedToken) res.sendStatus(404);
 
-    if (!decodedToken)
-        res.sendStatus(404);
+  /* Token valid, send them to a page to enter new password */
 
-    /* Token valid, send them to a page to enter new password */
+  // FOR TESTING. NEWPW..
+  if (decodedToken != null) resetPassword(decodedToken, req.params.newpw);
 
-    // FOR TESTING. NEWPW..
-    if (decodedToken != null)
-        resetPassword(decodedToken, req.params.newpw);
-    
-    res.send("Success");
-
+  res.send("Success");
 });
-
 
 export default emailRoute;
